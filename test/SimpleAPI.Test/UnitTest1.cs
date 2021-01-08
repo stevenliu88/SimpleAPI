9aecaa9990e8ef7f58a1e414331578d85572b2cc
stevenliu88 <30949641+stevenliu88@users.noreply.github.com>
2021-01-08 12:11:00 +1100

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void GetReturnName() 
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("steven", returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2,2);
+        }
+    }
+}
